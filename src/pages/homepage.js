6c5546624d1e25a9d6e3e5bb35ec3ec72bf9c6dd
kennja05 <jacobkenny05@gmail.com>
2021-05-02 15:17:26 -0400

    add images folder. begin homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,7 +1,25 @@
+import styled from 'styled-components'
+import trumpet from '../../src/assets/images/trumpet.jpg';
+
+const HomepageHeader = styled.header`
+    height: 100vh;
+    font-size: 2.1rem;
+    background: rgba(0, 0, 0, 0.5), url(${trumpet});
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    border: 1px solid red;
+`
+
 export default function Homepage(){
     return(
-        <h1>
-            PCWB Homepage
-        </h1>
+        <HomepageHeader>
+            <h1>
+                PCWB Homepage
+            </h1>
+        </HomepageHeader>
+        
     )
 }
