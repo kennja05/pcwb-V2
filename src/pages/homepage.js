9927656b0417a8616dc463af19defd78add8b9ad
kennja05 <jacobkenny05@gmail.com>
2021-06-08 22:29:42 -0400

    stack intro and main headers for main page

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,7 +5,27 @@ const IntroHeader = styled.header`
     height: 100vh;
     font-size: 2.1rem;
     background-color: ${props => props.darkBlue};
-    position: relative;
+`
+
+const IntroTitleBox = styled.div`
+    color: ${props => props.white};
+`
+
+const MainHeader = styled.header`
+    height: 100vh;
+    font-size: 2.1rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: url(https://images-na.ssl-images-amazon.com/images/I/71l6xKqHLCL._AC_SL1500_.jpg)
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    `
+
+const MainHeaderTitleBox = styled.div`
+    color: ${props => props.white};
 `
 
 export default class Homepage extends React.Component{
@@ -15,21 +35,27 @@ export default class Homepage extends React.Component{
     }
     
     componentDidMount(){
-        console.log(this.props)
-        setTimeout(() => this.setState({intro: false}), 5000)
+        // setTimeout(() => this.setState({intro: false}), 5000)
     }
 
     render(){
-        const {intro} = this.state
         return(
-            intro ? 
-            <IntroHeader colors={this.props.colors}>
-                <h1>
-                    PCWB Homepage
-                </h1>
-            </IntroHeader> 
-            :
-            <h1>poop</h1>  
+            <div style={{position: 'relative'}}>
+
+                <IntroHeader {...this.props}>
+                    <IntroTitleBox {...this.props}>
+                        PCWB Homepage
+                    </IntroTitleBox>
+                </IntroHeader> 
+
+                <MainHeader {...this.props}>
+                    <MainHeaderTitleBox {...this.props}>
+                        PCWB Homepage
+                    </MainHeaderTitleBox>
+                </MainHeader> 
+            </div>
+
+             
         )
     }
 }
