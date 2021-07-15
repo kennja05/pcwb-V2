2f08a8e0af92f388f5d7bf7d32e9cec31890d4cf
kennja05 <jacobkenny05@gmail.com>
2021-07-14 20:02:13 -0400

    add homepage content

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import TrumpetBackground from '../assets/images/trumpet.jpg'
-import styled, {keyframes} from 'styled-components'
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+
+import TrumpetBackground from '../assets/images/trumpet.jpg';
+import Seal from '../assets/images/seal.png';
 
 const HomepageWrapper = styled.div`
+    background-color: ${props => props.darkBlue};
+`
+
+const HomepageHeader = styled.header`
     text-align: center;
     display: grid;
     grid-template-columns: 100vw;
@@ -25,7 +31,7 @@ const lineDrawToLeft = keyframes`
     0% {left: 100%;width:0;}100% {left: 0;width:100%;}
 `
 //todo - take a look at timing of fadeOut animation
-const IntroHeader = styled.header`
+const IntroHeader = styled.div`
     grid-area: header;
     z-index: 1;
     background-color: ${props => props.darkBlue};
@@ -44,7 +50,7 @@ const IntroTitleBox = styled.div`
     &:before, &:after {
         content: '';
         width: 100%;
-        border-bottom: solid .5rem ${props => props.white};
+        border-bottom: solid .5rem ${props => props.orange};
         position: absolute;
     };
     &:before {
@@ -66,9 +72,9 @@ const IntroH2 = styled.h2`
     animation: ${fadeIn} 1.5s 2s backwards;
 `
 
-const MainHeader = styled.header`
+const MainHeader = styled.div`
     grid-area: header;
-    background-image: url(${TrumpetBackground});
+    background-image: linear-gradient(to right bottom, rgba(4, 4, 4, 0.5), rgba(4, 4, 4, 0.5)),url(${TrumpetBackground});
     background-attachment: fixed;
     background-position: center;
     background-repeat: no-repeat;
@@ -82,13 +88,13 @@ const MainHeader = styled.header`
 
 const MainHeaderTitleBox = styled.div`
     position: relative;
-    color: ${props => props.darkBlue};
+    color: ${props => props.white};
     line-height: 1.5;
     padding: 0 1.5rem 0 1.5rem;
     &:before, &:after {
         content: '';
         width: 100%;
-        border-bottom: solid .5rem ${props => props.darkBlue};
+        border-bottom: solid .5rem ${props => props.orange};
         position: absolute;
     };
     &:before {
@@ -104,19 +110,32 @@ const MainHeaderTitleBox = styled.div`
 export default function Homepage(props) {
     
     return(
-        <HomepageWrapper>
-            <IntroHeader {...props}>
-                <IntroTitleBox {...props}>
-                    <IntroH1>Penn Central Wind Band</IntroH1>
-                    <IntroH2>William Kenny, Conductor</IntroH2>
-                </IntroTitleBox>
-            </IntroHeader> 
-            <MainHeader {...props}>
-                <MainHeaderTitleBox {...props}>
-                    <h1>Penn Central Wind Band</h1>
-                    <h2>William Kenny, Conductor</h2>
-                </MainHeaderTitleBox>
-            </MainHeader> 
+        <HomepageWrapper {...props}>
+            <HomepageHeader>
+                <IntroHeader {...props}>
+                    <IntroTitleBox {...props}>
+                        <IntroH1>Penn Central Wind Band</IntroH1>
+                        <IntroH2>William Kenny, Conductor</IntroH2>
+                    </IntroTitleBox>
+                </IntroHeader> 
+                <MainHeader {...props}>
+                    <MainHeaderTitleBox {...props}>
+                        <h1>Penn Central Wind Band</h1>
+                        <h2>William Kenny, Conductor</h2>
+                    </MainHeaderTitleBox>
+                </MainHeader> 
+            </HomepageHeader>
+            <section>
+                <h2>About Us</h2>
+                <p>
+                    The Penn Central Wind Band is an all-volunteer organization sponsored by Bucknell University as a service to the region. Many members of the band live in central Pennsylvania, but some travel from far afield to rehearse and perform with the ensemble. Many of the band's members hold music degrees, and are music teachers and professional performers, athough all are great players!
+                </p>
+                <p>
+                    The PCWB, founded in 1994, is one of the few select ensembles to have been awarded the Silver Sudler Scroll, awarded by the John Philip Sousa Foundation "recognizing community and adult concert bands of outstanding musical excellence." Other honors include invitations to perform for the conference of the Pennsylvania Music Educators Association and the national convention of the Association of Concert Bands. William Kenny, the band's founder and conductor, is professor of music at Bucknell, where he has taught for the past 30 years.
+                </p>
+                <img src={Seal} alt />
+            </section>
         </HomepageWrapper>
+        
     )
 }
