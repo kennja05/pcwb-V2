db20f5421dd6cc4fb07b869522faffb1ac24b5a5
kennja05 <jacobkenny05@gmail.com>
2021-07-24 16:08:49 -0400

    add menu slide

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavBar = styled.nav`
     position: fixed;
     height: 100vh;
     top: 0;
-    left: 0;
+    left: ${props => props.active ? '0' : '-20rem'};
     width: 20rem;
+    transition: left 2s;
     display: flex;
     align-items: stretch;
     flex-direction: column; 
@@ -19,8 +20,8 @@ const NavBar = styled.nav`
     padding: 1rem 1rem 0 1rem;
     
     & a:nth-of-type(even){
-        border-bottom: .2rem solid black;
-        border-top: .2rem solid black;
+        border-bottom: .2rem solid #d55b37;
+        border-top: .2rem solid #d55b37;
     }
 
 `
@@ -28,7 +29,7 @@ const NavBar = styled.nav`
 export default class Nav extends React.Component {
 
     state = {
-        active: false
+        active: true
     };
 
     toggleMenu = () => {
@@ -39,11 +40,11 @@ export default class Nav extends React.Component {
 
     render(){
         return(
-            <NavBar {...this.props}>
-                <span onClick={this.toggleMenu}>×</span>
-                <Link to='/'>Homepage</Link>
-                <Link to='/director'>Director</Link>
-                <Link to='/media'>Media</Link>
+            <NavBar active={this.state.active} {...this.props}>
+                <button onClick={this.toggleMenu}>×</button>
+                <NavLink exact to='/'>Homepage</NavLink>
+                <NavLink exact to='/director'>Director</NavLink>
+                <NavLink exact to='/media'>Media</NavLink>
             </NavBar>
         )
     }
