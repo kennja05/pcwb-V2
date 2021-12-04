83ab49b3dddc3bc40d3817c381ab370ab459a03a
kennja05 <jacobkenny05@gmail.com>
2021-12-04 00:06:40 -0500

    update homepage article

--- src/partials/article.js
+++ src/partials/article.js
@@ -2,14 +2,23 @@ import styled from 'styled-components';
 
 export const StyledArticle = styled.article`
     max-width: calc(100% - 10rem);
-    border: 2px solid orange;
-    line-height: 2.5rem;
+    margin: 0 0 2rem 0;
+    line-height: 2.8rem;
+    & section h2{
+        color: ${props => props.theme.colors.orange};
+        padding: 2rem;
+        display: inline-block;
+        &:hover{
+            color: ${props => props.theme.colors.offWhite};
+        }
+    }
+    & section div {
+        background: ${props => props.theme.colors.offWhite};
+        padding: 1rem;
+        border-radius: .3rem;
+    }
     & section:nth-of-type(odd) {
         width: 65%;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        background: ${props => props.theme.colors.offWhite};
         color: ${props => props.theme.colors.darkBlue2};
     };
     & section:nth-of-type(even) {
--- src/partials/articlewrapper.tsx
+++ src/partials/articlewrapper.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ArticleWrapper = styled.div`
     color: ${props => props.theme.colors.offWhite};
     width: 100vw;
-    background: ${props => props.theme.colors.darkBlue};
+    background: ${props => props.theme.colors.darkBlue2};
     font-size: 2rem;
     display: flex;
     justify-content: center;
