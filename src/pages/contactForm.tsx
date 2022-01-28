cd28dd3e47e70ccdac5af82075fa6e86581a6e13
Jacob Kenny <jacobkenny05@gmail.com>
2022-01-28 15:53:12 -0500

    add form to contact page

--- src/pages/contactForm.tsx
+++ src/pages/contactForm.tsx
@@ -1,15 +1,33 @@
 import styled from 'styled-components'
 
-const FormWrapper = styled.div`
+const FormPageWrapper = styled.div`
     height: 100vh;
     width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     background-image: linear-gradient(${props => props.theme.colors.darkOrange}, ${props => props.theme.colors.orange});
 `
+const FormWrapper = styled.div`
+    background: ${props => props.theme.colors.offWhite};
+    border-radius: 1rem;
+    padding: 3rem;
+    box-shadow: 3px 3px ${props => props.theme.colors.darkBlue2};
+`
+
 
 export default function contactForm(){
     return (
-        <FormWrapper>
-
-        </FormWrapper>
+        <FormPageWrapper>
+            <FormWrapper>
+                <h1>Questions, Commments, or Inquiries</h1>
+               <form>
+                    <textarea></textarea>
+                    <input type='email' />
+                    <input type='submit' />
+                </form> 
+            </FormWrapper>
+        </FormPageWrapper>
     )
 }
