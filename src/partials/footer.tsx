e379cbfa98060800ba7f98dc4b4321b10cf9e981
Jacob Kenny <jacobkenny05@gmail.com>
2022-03-19 16:37:26 -0400

    update footer font color / border

--- src/partials/footer.tsx
+++ src/partials/footer.tsx
@@ -1,16 +1,24 @@
 import styled from 'styled-components'
 
 const Footer = styled.footer`
-    background: ${props => props.theme.colors.offWhite};
-    color: ${props => props.theme.colors.darkBlue2};
-
+    font-size: 1.5rem;
+    background: ${props => props.theme.colors.darkBlue};
+    border-top: .2rem solid ${props => props.theme.colors.darkBlue2};
+    padding: .5rem 1.5rem;
+    color: ${props => props.theme.colors.offWhite};
+    & span {
+        margin-right: .5rem;
+    }
+    & a:hover {
+        color: ${props => props.theme.colors.orange};
+    }
 `
 
 export default function footer() {
     return (
-        <Footer>
+        <Footer onClick={(e) => console.log(e.target)}>
             <span>©2022</span>
-            <span><a href='/contact'>Contact Us</a></span>
+            <span><a href='/contact'>Contact</a></span>
         </Footer>
     )
 }
